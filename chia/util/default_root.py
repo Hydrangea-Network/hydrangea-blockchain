d6bfc10e42fe94d3f828cf40b90e1e976311f6a9
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-03-19 09:35:27 -0500

    set root paths

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("HYDRANGEA_ROOT", "~/.chia/mainnet"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("HYDRANGEA_ROOT", "~/.hydrangea/mainnet"))).resolve()
 STANDALONE_ROOT_PATH = Path(
-    os.path.expanduser(os.getenv("CHIA_STANDALONE_WALLET_ROOT", "~/.chia/standalone_wallet"))
+    os.path.expanduser(os.getenv("HYDRANGEA_STANDALONE_WALLET_ROOT", "~/.hydrangea/standalone_wallet"))
 ).resolve()
 
-DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("HYDRANGEA_KEYS_ROOT", "~/.hydrangea_keys"))).resolve()
