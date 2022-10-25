1f14edd1d3e6a56b794a8f280bcddd19e4aaf08b
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-10-25 15:09:34 -0500

    Update GUI name

--- build_scripts/build_linux_deb-2-installer.sh
+++ build_scripts/build_linux_deb-2-installer.sh
@@ -57,10 +57,10 @@ ln -s ../../opt/chia/chia "dist/$CLI_DEB_BASE/usr/bin/chia"
 dpkg-deb --build --root-owner-group "dist/$CLI_DEB_BASE"
 # CLI only .deb done
 
-cp -r dist/daemon ../chia-blockchain-gui/packages/gui
+cp -r dist/daemon ../hydrangea-blockchain-gui/packages/gui
 
 # Change to the gui package
-cd ../chia-blockchain-gui/packages/gui || exit 1
+cd ../hydrangea-blockchain-gui/packages/gui || exit 1
 
 # sets the version for chia-blockchain in package.json
 cp package.json package.json.orig
